46f9918bfa43f69162b72de87f6b94c15d93283f
jayashri1983 <jayashri.phonde@dmce.ac.in>
2025-08-05 18:48:57 +0530

    Create vulnerable.py

---
+++ vulnerable.py
@@ -0,0 +1,10 @@
+import os
+
+def run_ping(user_input):
+    # 🚨 Vulnerable: unsanitized input leads to command injection
+    command = f"ping -c 1 {user_input}"
+    os.system(command)
+
+if __name__ == "__main__":
+    user_input = input("Enter IP address or hostname: ")
+    run_ping(user_input)
